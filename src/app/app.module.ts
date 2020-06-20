76fb207808c8ad0e9634bfcd317d6d68779b9d7f
Cristhian Ortiz <cristhianortizmercado@gmail.com>
2020-06-20 15:23:56 -0400

    corrgiendo un el problema forRoot para que lazy divida archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,6 @@ import { HomeModule } from './home/home.module';
     FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
